b3d5935fedb9df78342dbe6f9405c56d87d4728a
Ahmad Damra <ahmeddamra_cs@hotmail.com>
2024-09-28 13:53:54 +0300

    Add project files.

---
+++ ArtiAPI/Models/Employee.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ArtiAPI.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string City { get; set; }
+    }
+}
